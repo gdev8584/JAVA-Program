d0cdbfcae6d978685b58cae38fec6b52ed95a60c
DEVANAND GIRI <102689281+gdev8584@users.noreply.github.com>
2022-04-18 19:49:31 +0530

    Update Variables & Operators, Datatypes & Variables, Operators, Ternary Operator.java

--- Variables & Operators, Datatypes & Variables, Operators, Ternary Operator.java
+++ Variables & Operators, Datatypes & Variables, Operators, Ternary Operator.java
@@ -41,3 +41,54 @@ class Main {
 
 	}
 }
+
+/* Data types
+Problem Statement
+Some Data types are given below:-
+Integer
+Long
+float
+Double
+char
+Your task is to take input in the given format and print them in the same order.
+Input
+The input contains the following values separated by lines:- integer, Long, float, double, char
+Output
+Print each element in a new line in the same order as input.
+Example
+Sample Input:-
+2
+2312351235
+1.21
+543.1321
+c
+
+Sample Output:-
+2
+2312351235
+1.21
+543.1321
+c
+*/
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                      // Your code here
+					  Scanner s= new Scanner(System.in);
+					  int i= s.nextInt();
+					  long l = s.nextLong();
+					  float f = s.nextFloat();
+					  double d = s.nextDouble();
+					  char c = s.next().charAt(0);
+
+					  System.out.println(i);
+					  System.out.println(l);
+					  System.out.println(f);
+					  System.out.println(d);
+					  System.out.println(c);
+	}
+}
